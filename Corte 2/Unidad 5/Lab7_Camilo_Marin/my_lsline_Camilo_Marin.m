ad6370997b4e8a393ce72cf3e12aaec4a5468b03
Camilo <juancmaringcu@gmail.com>
2020-07-24 22:39:38 -0500

    Lab7 final

--- Corte 2/Unidad 5/Lab7_Camilo_Marin/my_lsline_Camilo_Marin.m
+++ Corte 2/Unidad 5/Lab7_Camilo_Marin/my_lsline_Camilo_Marin.m
@@ -4,12 +4,12 @@ Y=sum(y);%Sumatoria de y
 XX=sum(x.^2);%Sumatoria de x^2
 XY=sum(x.*y);%Sumatoria de x*y
 m=length(x);
-[a, b]= minimosCuadrados(X,Y,XX,XY,m);
+[a, b]= cohe(X,Y,XX,XY,m);
 end
 
 %Calculando recta de minimos cuadrados
 
-function [a,b] = minimosCuadrados(X,Y,XX,XY,m)
+function [a,b] = cohe(X,Y,XX,XY,m)
 a=(m*XY-X*Y)/(m*XX-X^2);
 b=(Y*XX-X*XY)/(m*XX-X^2);
 end
